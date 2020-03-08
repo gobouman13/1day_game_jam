148c76c6299ff76893ff958eaf0f865135720471
Okamoto999 <okamoto.yuuki.infratop@gmail.com>
2020-03-08 15:39:32 +0900

    モブスポーン機能製作開始、幾つかのバグの修正

---
+++ Assets/MobSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawn : MonoBehaviour
+{
+    [SerializeField] private GameObject MobBase;
+    [SerializeField] private GameObject Player;
+
+    [SerializeField] private Vector3[] spawnPoints;
+
+    [SerializeField] private int firstMobNum;
+
+    [SerializeField] private GameObject MobGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnMob()
+    {
+        bool chk = false;
+        Vector3 pos=new Vector3();   
+    }
+}
