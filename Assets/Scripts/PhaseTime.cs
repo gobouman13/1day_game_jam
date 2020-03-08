f0a3ed413935adc1e083e6899d94a3ff48bd410b
minion7777 <asami12motoki15@gmail.com>
2020-03-08 15:37:10 +0900

    再プッシュ

--- Assets/Scripts/PhaseTime.cs
+++ Assets/Scripts/PhaseTime.cs
@@ -11,20 +11,22 @@ public class PhaseTime : MonoBehaviour
 
     public Text timetext;
 
+
     // Start is called before the first frame update
     void Start()
     {
-        time = 20.00f;        
+        time = 20.00f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PhaseTimer();
     }
     public void PhaseTimer()
     {
         time -= Time.deltaTime;
         timetext.text = time.ToString("f2");
+
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,71 +4,82 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-	public GameObject mainCamera;
-
 	public float speed;
 	float angle,direction;
-	float weaponDirection;
-
-	bool frozen;
-
     // Start is called before the first frame update
     void Start()
     {
-        mainCamera = Camera.main.gameObject;
         speed=100;
-		frozen = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-		PlayerMove();
-		PlayerRotation();
-        PlayerAtk();
+        PlayerMove();
     }
-
-
 	void PlayerMove()
 	{
-        if(!frozen){
-			float x = Input.GetAxis("Horizontal");
-			float y = Input.GetAxis("Vertical");
-			GetComponent<Rigidbody2D>().velocity = new Vector2(x * speed * Time.deltaTime, y * speed * Time.deltaTime);
-		}else GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
-		mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
-	}
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+		GetComponent<Rigidbody2D>().velocity = new Vector2(x * speed * Time.deltaTime, y * speed * Time.deltaTime);
 
-	void PlayerRotation()
-	{
-        if(!frozen){
-			var screenPos = Camera.main.WorldToScreenPoint( transform.position );
-			var dir = Input.mousePosition - screenPos;
-			angle = GetAim( Vector3.zero, dir);
-			transform.GetChild(0).transform.rotation = Quaternion.Euler(0,0,angle - 90);
-		}
-	}
+        var screenPos = Camera.main.WorldToScreenPoint( transform.position );
+        var dir = Input.mousePosition - screenPos;
+        angle = GetAim( Vector3.zero, dir);
 
-	void PlayerAtk()
-	{
-        if (Input.GetKeyDown(KeyCode.Z) && !frozen){
-			StartCoroutine(MoveWeapon());
-		}
-	}
 
-	public IEnumerator MoveWeapon()
-    {
-        transform.GetChild(1).gameObject.SetActive(true);
-		frozen = true;
-		weaponDirection = 60;
-		for(int i = 0;i < 15;i++){
-			transform.GetChild(1).transform.rotation = Quaternion.Euler(0,0,angle + weaponDirection - 90);
-			weaponDirection -= 8;
-			yield return new WaitForSeconds(0.01f);
-		}
-        transform.GetChild(1).gameObject.SetActive(false);
-		frozen = false;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        transform.GetChild(0).transform.rotation = Quaternion.Euler(0,0,angle + 90);
+	}
 
     public float GetAim( Vector2 from, Vector2 to )
     {
