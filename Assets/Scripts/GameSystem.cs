c62ac37c09897c393c16504e4fc8d0d7e7a03d5c
Okamoto999 <okamoto.yuuki.infratop@gmail.com>
2020-03-08 11:48:22 +0900

    Scriptsフォルダ作成、モードチェンジ機能実装

---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystem : MonoBehaviour
+{
+    [SerializeField] private float[] defaultModeTime;
+
+    private float nowModeTime;
+
+    [SerializeField] private int nowModeLength;
+    private int nowMode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ChkModeTime();   
+    }
+
+    void ChkModeTime()
+    {
+        nowModeTime += Time.deltaTime;
+        if (nowModeTime > defaultModeTime[nowMode])
+        {
+            nowModeTime -= defaultModeTime[nowMode];
+            nowMode++;
+            if (nowMode >= nowModeLength) nowMode = 0;
+            Debug.Log("Changed Mode");
+        }
+    }
+}
