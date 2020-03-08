afcdc9d3387e4de836366413d78d0547f617921e
Okamoto999 <okamoto.yuuki.infratop@gmail.com>
2020-03-08 16:03:26 +0900

    MobSpawn製作中

--- Assets/MobSpawn.cs
+++ Assets/MobSpawn.cs
@@ -12,6 +12,12 @@ public class MobSpawn : MonoBehaviour
     [SerializeField] private int firstMobNum;
 
     [SerializeField] private GameObject MobGroup;
+
+    [SerializeField] private float spawnMobInterval;
+    
+    private GameObject obj;
+
+    float nowSpawnMobInterval;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +33,28 @@ public class MobSpawn : MonoBehaviour
     public void SpawnMob()
     {
         bool chk = false;
-        Vector3 pos=new Vector3();   
+        Vector3 pos=new Vector3();
+
+        while (!chk)
+        {
+            float distanse = Random.Range(700f, 500f);
+            float degree = Random.Range(0f, 360f);
+            pos = new Vector3(Mathf.Sin(Mathf.Deg2Rad*degree),Mathf.Cos(Mathf.Deg2Rad*degree))*distanse;
+            Vector2 ans = pos;
+            if (Mathf.Abs(pos.x)<37f&&Mathf.Abs(pos.y)<37f) chk = true;
+        }
+        obj = Instantiate(MobBase, pos, Quaternion.identity);
+//        obj.GetComponent<AIDestinationSetter>().target = Player.transform;
+    }
+
+    IEnumerator FirstSpawn()
+    {
+        yield return new WaitForSeconds(3);
+        for (int i = 0; i < firstMobNum; i++)
+        {
+            SpawnMob();
+            yield return new WaitForSeconds(spawnMobInterval);
+        }
+        
     }
 }
